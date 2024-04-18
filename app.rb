0f23575e8b04b8659f4c2671bf052c4b9d6ac488
sutantoc <165302919+sutantoc@users.noreply.github.com>
2024-04-18 03:49:01 +0000

    payment and results

--- app.rb
+++ app.rb
@@ -22,3 +22,22 @@ get("/square_root/results") do
   @user_number = params.fetch("user_number").to_i
   erb(:square_root_results)
 end
+
+get("/payment/new") do
+  erb(:payment)
+end
+
+get("/payment/results") do
+  @user_APR = params.fetch("user_APR").to_f
+  @user_years = params.fetch("user_years").to_i
+  @user_pv = params.fetch("user_pv").to_f
+
+  @r = @user_APR / 1200
+  @numerator = @r * @user_pv
+  @neg_periods = @user_years * -12
+  @r_plus1 = @r + 1
+  @power_n = @r_plus1 ** @neg_periods
+  @denominator = 1 - @power_n
+  @payment = @numerator / @denominator
+  erb(:payment_results)
+end
