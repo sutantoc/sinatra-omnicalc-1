1344997c854505c7091100ad458c705ab986e996
sutantoc <165302919+sutantoc@users.noreply.github.com>
2024-04-18 03:28:53 +0000

    square root new and results

--- app.rb
+++ app.rb
@@ -9,8 +9,16 @@ get("/square/new") do
   erb (:square)
 end
 
-
 get("/square/results") do
   @number = params.fetch("number").to_i
   erb (:square_results)
+end
+
+get("/square_root/new") do
+  erb(:square_root)
+end
+
+get("/square_root/results") do
+  @user_number = params.fetch("user_number").to_i
+  erb(:square_root_results)
 end
