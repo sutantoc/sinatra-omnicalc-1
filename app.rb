2562b890fad917b3df72c9b9df46f0c293f6ea3b
sutantoc <165302919+sutantoc@users.noreply.github.com>
2024-04-18 03:11:07 +0000

    square new

--- app.rb
+++ app.rb
@@ -2,8 +2,14 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:square)
+end
+
+get("/square/new") do
+  erb (:square)
+end
+
+
+get("square/results") do
+  erb (:square_results)
 end
